d11d3e2953ef3207c498e6bd755d932c872a6714
MananTaneja <manantaneja007@gmail.com>
2024-12-28 11:10:22 +0530

    fix: wezterm config and added split pane functionality

--- configs/.wezterm.lua
+++ configs/.wezterm.lua
@@ -8,8 +8,8 @@ config.color_scheme = 'Builtin Tango Dark'
 config.max_fps = 120
 
 -- Font
-config.font = wezterm.font("JetBrains Mono")
-config.font_size = 14
+config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
+config.font_size = 15
 
 -- Tab Bar
 config.use_fancy_tab_bar = false
@@ -18,9 +18,12 @@ config.tab_bar_at_bottom = true
 
 -- Window
 config.window_decorations = "RESIZE"
+config.initial_rows = 48
+config.initial_cols = 150
 
--- Key Bindings - Natural Text Editing
-config.keys = {{
+-- Key Bindings
+config.keys = { -- Natural Text Editing
+{
     mods = "OPT",
     key = "LeftArrow",
     action = action.SendKey({
@@ -55,17 +58,57 @@ config.keys = {{
         mods = "CTRL",
         key = "u"
     })
-}, {
-    mods = "CMD",
-    key = "k",
-    action = action.ClearScrollback 'ScrollbackAndViewport'
-}}
-
--- Key Bindings - Custom shortcuts
-config.keys = {{
+}, -- Custom Shortcuts
+{
     mods = "CTRL",
     key = "d",
     action = action.SendString 'cd "$(find ~/Developer -type d -maxdepth 1 | fzf)"\r'
+}, -- Split Panes
+{
+    key = 'd',
+    mods = "CMD",
+    action = wezterm.action.SplitHorizontal {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'h',
+    mods = "CMD",
+    action = wezterm.action.SplitVertical {
+        domain = 'CurrentPaneDomain'
+    }
+}, {
+    key = 'w',
+    mods = "CMD",
+    action = wezterm.action.CloseCurrentPane {
+        confirm = true
+    }
+}, {
+    key = 'LeftArrow',
+    mods = 'CMD',
+    action = action.ActivatePaneDirection 'Left'
+}, {
+    key = 'RightArrow',
+    mods = 'CMD',
+    action = action.ActivatePaneDirection 'Right'
+}, {
+    key = 'UpArrow',
+    mods = 'CMD',
+    action = action.ActivatePaneDirection 'Up'
+}, {
+    key = 'DownArrow',
+    mods = 'CMD',
+    action = action.ActivatePaneDirection 'Down'
+}, -- Clear Scrollback and Search
+{
+    mods = "CMD",
+    key = "k",
+    action = action.ClearScrollback 'ScrollbackAndViewport'
+}, {
+    mods = "CMD",
+    key = 'f',
+    action = action.Search {
+        CaseInSensitiveString = ''
+    }
 }}
 
 config.audible_bell = 'Disabled'
